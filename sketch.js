1ef1e906b7633608d00b808fd90a2fb630a9e585
fe1zed <112386300+fe1zed@users.noreply.github.com>
2024-11-18 10:52:16 -0600

    Init

---
+++ sketch.js
@@ -0,0 +1,15 @@
+// CS30 Major Project
+// Dmitrii Pletmintsev
+// 11/18/24
+//
+// Extra for Experts:
+// - describe what you did to take this project "above and beyond"
+
+
+function setup() {
+  createCanvas(windowWidth, windowHeight);
+}
+
+function draw() {
+  background(220);
+}
