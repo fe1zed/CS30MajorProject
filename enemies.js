96c926d6841034d574ddc4a7bd80df70144c2fab
fe1zed <112386300+fe1zed@users.noreply.github.com>
2024-12-13 10:58:11 -0600

    Attack Varieties

--- enemies.js
+++ enemies.js
@@ -166,7 +166,7 @@ class Enemy {
             return;
         }
 
-        let states = ["idle", "move", "attack"];
+        let states = ["attack"]; //["idle", "move", "attack"];
         this.state = random(states);
         this.currentTimeBetweenStates = 0;
         this.timeBetweenStates = this.state === "attack"? 750 : random(this.minTimeBetweenStates, this.maxTimeBetweenStates);
@@ -177,10 +177,15 @@ class Enemy {
 
     }
 
+    displayBullets() {
+
+    }
+
     render() {
         this.applyState();
         this.changeState();
         this.display();
+        this.displayBullets();
         if (this.alive) {
             this.displayHealthBar();
         }
@@ -210,6 +215,9 @@ class VarkolynLeader  extends Enemy {
         super(x, y, pixelWidth, pixelHeight, health);
 
         this.enemyBulletImage = null;
+        this.bullets = [];
+        this.bulletAmount = 5;
+        this.bulletsDeley = 0.1;
     }
 
     attack() {
@@ -223,6 +231,26 @@ class VarkolynLeader  extends Enemy {
 
     loadAdditionalData() {
         this.enemyBulletImage = loadImage("Enemies/Sprites/EnergySphere.gif");
+    }
+
+    createBullet() {
+        return new SinusoidalAttack(this.bulletStartX, this.bulletStartY, 20, 20);
+    }
+
+    shootBullet() {
+        // Make deley for spwaning bullets
+        for (let i = 0; i < this.bulletAmount; i++) {
+            console.log(`Creating ${this.bulletAmount} bullets`);
+            this.bulletStartX = this.x + this.pixelWidth * 2 / 3 + 125;
+            this.bulletStartY = this.y + 125;
+            this.bullets.push(this.createBullet()); 
+        }
+    }
+
+    displayBullets() {
+        for (let bullet of this.bullets) {
+            bullet.render();
+        }
     }
 }
 
@@ -244,3 +272,42 @@ window.PhantomKing = PhantomKing;
 window.VarkolynLeader = VarkolynLeader;
 window.ChristmasTreant = ChristmasTreant;
 window.Nian = Nian;
+
+// Attack variaties
+class StraightAttack {
+
+}
+
+class SinusoidalAttack {
+    constructor (x, y, bulletWidth, bulletHeight, speed) {
+        this.x = x;
+        this.y = y;
+        this.bulletWidth = bulletWidth;
+        this.bulletHeight = bulletHeight;
+        this.bulletSpeed = 5;
+    }
+
+    display() {
+        fill("red");
+        circle(this.x, this.y, this.bulletWidth);
+    }
+
+    move() {
+        push();
+        translate(this.x, this.y);
+        this.x += this.bulletSpeed;
+        this.y = 75 * sin(this.x / 100) + 125 / 2;
+        this.y += enemies[0].y;
+        pop();
+    }
+
+    render() {
+        this.move();
+        this.display();
+    }
+}
+
+
+class Lazer {
+    
+}
